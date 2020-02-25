e0be91eed3ff22d0bf0638735528412317e068b7
Bianca Herbst <b.herbst@posteo.de>
2020-02-25 15:28:15 +0100

    add validations to cutie model

--- app/models/cutie.rb
+++ app/models/cutie.rb
@@ -2,4 +2,8 @@ class Cutie < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :description, presence: true
+  validates :booking_rate, presence: true
 end
